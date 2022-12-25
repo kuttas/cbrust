fee456403b6c618d92debd16d4cd39249b60dd6a
Kutta Srinivasan <kutta@linkedin.com>
2022-12-25 13:57:49 -0700

    basic mysql example using local, default, insecure mysql instance

--- cbserver/src/main.rs
+++ cbserver/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use mysql::*;
+use mysql::prelude::*;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Database {
+    name: String
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let url = "mysql://root@localhost:3306";
+    let pool = Pool::new(url)?;
+    let mut conn = pool.get_conn()?;
+
+    let dbs = conn
+        .query_map(
+            "SHOW DATABASES;",
+            |name| {
+                Database { name }
+            },
+        )?;
+
+
+    println!("SHOW DATABASES result:\n{:?}", dbs);
+
+    Ok(())
 }
