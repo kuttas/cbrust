bad27a7ed972a9cbe07314a2a6a623f2931e7013
Kutta Srinivasan <kutta@linkedin.com>
2022-12-26 17:24:12 -0700

    apply auto-format on save

--- cbcli/src/main.rs
+++ cbcli/src/main.rs
@@ -1,24 +1,23 @@
+use cbservice::{compute_broker_client::ComputeBrokerClient, GetHostInfoRequest};
 use std::io::stdin;
-use cbservice::{GetHostInfoRequest, compute_broker_client::{ComputeBrokerClient}};
 
 pub mod cbservice {
     tonic::include_proto!("cbservice");
 }
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let mut client = ComputeBrokerClient::connect("http://[::1]:8080").await?;
-  loop {
-    println!("\nPlease enter a hostname");
-    let mut hostname = String::new();
-    stdin().read_line(&mut hostname).unwrap();
-    let hostname = hostname.trim();
+    let mut client = ComputeBrokerClient::connect("http://[::1]:8080").await?;
+    loop {
+        println!("\nPlease enter a hostname");
+        let mut hostname = String::new();
+        stdin().read_line(&mut hostname).unwrap();
+        let hostname = hostname.trim();
 
-    let request = tonic::Request::new(GetHostInfoRequest {
-        hostname: String::from(hostname),
-      });
-    let response = client.get_host_info(request).await?;
-      println!("CB service says: '{}'", response.into_inner().info);
-  }
+        let request = tonic::Request::new(GetHostInfoRequest {
+            hostname: String::from(hostname),
+        });
+        let response = client.get_host_info(request).await?;
+        println!("CB service says: '{}'", response.into_inner().info);
+    }
 }
