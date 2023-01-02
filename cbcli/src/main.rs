bc05894f0630b2d9df86be3e5fd6d9616b56ea1e
Kutta Srinivasan <kutta@linkedin.com>
2023-01-01 17:48:12 -0700

    re-write CLI with 'clap' to do cmdline parsing, impl add-host

--- cbcli/src/main.rs
+++ cbcli/src/main.rs
@@ -1,28 +1,76 @@
-use cbprotolib::{compute_broker_client::ComputeBrokerClient, AddHostRequest};
-use std::io::stdin;
+use cbprotolib::compute_broker_client::ComputeBrokerClient;
+use clap::{Args, Parser, Subcommand};
+
+/// Command-Line Interface (CLI) for Compute Broker
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Lists hosts in Compute Broker's database
+    ListHosts(ListHosts),
+
+    /// Gets detailed info about a host in Compute Broker's database
+    GetHostInfo(GetHostInfo),
+
+    /// Adds hosts to Compute Broker's database
+    AddHost(AddHost),
+}
+
+#[derive(Args)]
+struct ListHosts {}
+
+#[derive(Args)]
+struct GetHostInfo {
+    /// Fully qualified domain name of host, e.g. `foobar.example.com`
+    hostname: String,
+}
+
+#[derive(Args)]
+struct AddHost {
+    /// Fully qualified domain name of host, e.g. `foobar.example.com`
+    hostname: String,
+}
+
+async fn list_hosts() {
+    // TODO: implement
+}
+
+async fn get_host_info(args: GetHostInfo) -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = ComputeBrokerClient::connect("http://[::1]:8080").await?;
+    let hostname = args.hostname.trim();
+    let request = tonic::Request::new(cbprotolib::GetHostInfoRequest {
+        hostname: String::from(hostname),
+    });
+    let response = client.get_host_info(request).await?;
+    println!("host info:\n{}", response.into_inner().info);
+    Ok(())
+}
+
+async fn add_host(args: AddHost) -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = ComputeBrokerClient::connect("http://[::1]:8080").await?;
+    let hostname = args.hostname.trim();
+    let request = tonic::Request::new(cbprotolib::AddHostRequest {
+        hostname: String::from(hostname),
+        info: String::from("todo useful info"),
+    });
+    let _ = client.add_host(request).await?;
+    println!("added host '{}'", hostname);
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ComputeBrokerClient::connect("http://[::1]:8080").await?;
-    loop {
-        println!("\nPlease enter a hostname");
-        let mut hostname = String::new();
-        stdin().read_line(&mut hostname).unwrap();
-        let hostname = hostname.trim();
+    let args = CliArgs::parse();
 
-        // TODO: fix the CLI to use actual flags/options so we can do CRUD operations.
-        // let request = tonic::Request::new(GetHostInfoRequest {
-        //     hostname: String::from(hostname),
-        // });
-        // let response = client.get_host_info(request).await?;
-        // println!("CB service says: '{}'", response.into_inner().info);
-
-        // Add host to DB with bogus info string
-        let request = tonic::Request::new(AddHostRequest {
-            hostname: String::from(hostname),
-            info: String::from("todo useful info"),
-        });
-        let response = client.add_host(request).await?;
-        println!("CB service success: {:?}", response);
+    match args.command {
+        Commands::ListHosts(_) => list_hosts().await,
+        Commands::GetHostInfo(args) => get_host_info(args).await?,
+        Commands::AddHost(args) => add_host(args).await?,
     }
+    Ok(())
 }
